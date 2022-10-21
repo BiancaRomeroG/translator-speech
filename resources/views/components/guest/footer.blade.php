8d640285dc91e28b850e042eff6447624b817bdd
unknown <edson3103a@gmail.com>
2022-10-21 00:06:50 -0400

    Vista Login y registro funcional

---
+++ resources/views/components/guest/footer.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <footer class="bg-white py-6 border-t border-gray-200">
+        <div class="container mx-auto px-4 xl:max-w-6xl ">
+            <div class="mx-auto px-4">
+                <div class="flex flex-wrap flex-row -mx-4">
+                    <div
+                        class="flex-shrink max-w-full px-4 w-full md:w-1/2 text-center md:ltr:text-left md:rtl:text-right">
+                        <ul class="ltr:pl-0 rtl:pr-0">
+                            <li class="inline-block ltr:mr-5 rtl:ml-3 ml-5">
+                                <a class="hover:text-indigo-500" href="login-ilustration.html#">Soporte</a>
+                            </li>
+                            <li class="inline-block ltr:mr-5 rtl:ml-3 ml-5">
+                                <a class="hover:text-indigo-500" href="login-ilustration.html#">Centro de ayuda</a>
+                            </li>
+                            <li class="inline-block ltr:mr-5 rtl:ml-3 ml-5">
+                                <a class="hover:text-indigo-500" href="login-ilustration.html#">Privacidad</a>
+                            </li>
+                            <li class="inline-block ltr:mr-5 rtl:ml-3 ml-5">
+                                <a class="hover:text-indigo-500" href="login-ilustration.html#">T&eacute;rminos de Servicio</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div
+                        class="flex-shrink max-w-full px-4 w-full md:w-1/2 text-center md:ltr:text-right md:rtl:text-left">
+                        <p class="mb-0 mt-3 md:mt-0">
+                            <a href="../index.html" class="hover:text-indigo-500">TranslatorSpeech</a> | Todos los derechos reservados &reg;
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,21 +1,58 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>@yield('title')</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
+    {{-- Tailwind --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- Estilos personalizados --}}
+    <link rel="stylesheet" href="{{ asset('css/colors.css') }}" type="text/css">
+
+    <!-- Development css (used in all pages) -->
+    <link rel="stylesheet" id="stylesheet" href="{{ asset('src/css/style.css') }}">
+    <!-- Production css (used in all pages) -->
+    <link rel="stylesheet" href="{{ asset('dist/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('src/css/customizer.css') }}">
+
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&amp;display=swap" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-base font-normal text-gray-600">
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+
+    <!--start::Global javascript (used in all pages)-->
+    <script src="{{ asset('vendors/alpinejs/dist/cdn.min.js') }}"></script><!-- core js -->
+    <script src="{{ asset('vendors/chart.js/dist/chart.min.js') }}"></script><!-- charts -->
+    <script src="{{ asset('vendors/flatpickr/dist/flatpickr.min.js') }}"></script><!-- input date -->
+    <script src="{{ asset('vendors/flatpickr/dist/plugins/rangePlugin.js') }}"></script><!-- input range date -->
+    <script src="{{ asset('vendors/@yaireo/tagify/dist/tagify.min.js') }}"></script><!-- input tags -->
+    <script src="{{ asset('vendors/pristinejs/dist/pristine.min.js') }}"></script><!-- form validation -->
+    <script src="{{ asset('vendors/simple-datatables/dist/umd/simple-datatables.js') }}"></script>
+    <!--sort table-->
+    <script src="{{ asset('vendors/glightbox/dist/js/glightbox.min.js') }}"></script><!-- lightbox image -->
+    <script src="{{ asset('vendors/flickity/dist/flickity.pkgd.min.js') }}"></script><!-- slider -->
+    <!--end::Global javascript (used in all pages)-->
+
+    <!-- Minify Global javascript (for production purpose) -->
+    <script src="{{ asset('dist/js/scripts.js') }}"></script>
+
+    <!--start::Demo javascript ( initialize global javascript )-->
+    <script src="{{  asset('src/js/demo.js') }}"></script>
+
+    <!--start::Customizer js ( Only for demo purpose )-->
+    <script src="{{  asset('src/js/customizer.js') }}"></script>
+</body>
+
 </html>
